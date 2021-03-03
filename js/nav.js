ea1ed05290cd267dadcb03d4c5978b90761e7966
Victoria1900 <victoriadmitrievna@bk.ru>
2021-03-03 17:28:22 +0500

    mobile

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,23 @@
 const navHeader = document.querySelector('.header-nav')
 const navButton = document.querySelector('.header-nav-wraper-button')
+const navClose = document.querySelector('.header-nav-button')
 
 navButton.addEventListener('click', function () {
     if (navHeader.classList.contains('nav-closed')) {
         navHeader.classList.remove('nav-closed')
         navHeader.classList.add('nav-opened')
     } else {
+
         navHeader.classList.add('nav-closed')
         navHeader.classList.remove('nav-opened')
+    }
+
+})
+
+navClose.addEventListener('click', function () {
+    if (navHeader.classList.contains('nav-opened')) {
+        navHeader.classList.remove('nav-opened')
+        navHeader.classList.add('nav-closed')
     }
 
 })
